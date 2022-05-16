85eca713b3b1293e014d7771d71ab6a12ca2841b
akshayn10 <thiruakshayan98@gmail.com>
2022-05-17 05:26:36 +0530

    Auth components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { HeaderComponent } from './header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SignupComponent,
+    LoginComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/auth/signup/signup.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SignupRequestPayload } from './signup-request.payload';
+import { AuthService } from '../shared/auth.service';
+// import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css'],
+  providers: [AuthService]
+})
+export class SignupComponent implements OnInit {
+
+  signupRequestPayload: SignupRequestPayload;
+  signupForm: FormGroup=new FormGroup({});
+
+  constructor(private authService: AuthService, private router: Router,private fb:FormBuilder
+    // private toastr: ToastrService
+    ) {
+    this.signupRequestPayload = {
+      username: '',
+      email: '',
+      password: ''
+    };
+  }
+
+  ngOnInit() {
+    this.signupForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  signup():void {
+    this.signupRequestPayload.email = this.signupForm.value.email;
+    // this.signupRequestPayload.username = this.signupForm.get('username').value;
+    this.signupRequestPayload.username = this.signupForm.value.username;
+    this.signupRequestPayload.password = this.signupForm.value.password;
+
+    this.authService.signup(this.signupRequestPayload)
+      .subscribe(data => {
+        this.router.navigate(['/login'],
+          { queryParams: { registered: 'true' } });
+      }, error => {
+        console.log(error);
+        // this.toastr.error('Registration Failed! Please try again');
+      });
+  }
+}
