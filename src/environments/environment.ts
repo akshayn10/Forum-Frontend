d87afb90595208c836788ca3a7e1f6da626770ca
akshayan <thiruakshayan98@gmail.com>
2022-11-13 20:19:52 +0530

    url change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  authServerUrl: 'http://localhost:8080/api/',
-  postServerUrl: 'http://localhost:8081/api/',
-  commentServerUrl: 'http://localhost:8082/api/'
+  authServerUrl: 'http://netprog-377908240.us-east-1.elb.amazonaws.com:8080/api/',
+  postServerUrl: 'http://netprog-377908240.us-east-1.elb.amazonaws.com:8081/api/',
+  commentServerUrl: 'http://netprog-377908240.us-east-1.elb.amazonaws.com:8082/api/'
 };
 
 /*
